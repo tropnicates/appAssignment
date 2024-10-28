798c2948d54914ecab252a21a1decfeae949a927
Kundan Kumar <kun9577@gmail.com>
2024-10-29 01:53:42 +0530

    added package for vercel

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './tapp.tsx';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { registerRootComponent } from 'expo';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'),
+});
