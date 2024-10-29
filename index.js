96a75c86146240e65cf41f00f04f5d9ea3861760
Kundan Kumar <kun9577@gmail.com>
2024-10-29 10:28:19 +0530

    image added in readme

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import { AppRegistry } from 'react-native';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './tapp.tsx';
-import { name as appName } from './app.json';
-import { registerRootComponent } from 'expo';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById('app-root'),
-});
